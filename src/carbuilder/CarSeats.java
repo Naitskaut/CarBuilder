416f2266d6008570679b349e0d4de93d1d13e585
yeska <eskendirabraim@gmail.com>
2026-09-09 14:12:24 +0500

    Add CarSeats

---
+++ src/carbuilder/CarSeats.java
@@ -0,0 +1,11 @@
+package carbuilder;
+
+public final class CarSeats {
+
+    public static final int MIN_SEATS = 2;
+    public static final int MAX_SEATS = 7;
+
+    private CarSeats(){
+
+    }
+}
